744a9567ae27d28827634ace1792ddf5383a1f09
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-16 15:04:25 +0530

    Time: 13 ms (70.82%), Space: 12.6 MB (35.89%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-    
-    int rec(int &ans,vector<int>&nums,int sum,int n,int target)
+    int rec(vector<vector<int>>&t,vector<int>&nums,int n,int sum)
     {
-        if(n==0 && sum==target)
+        if(t[n][sum]!=-1) return t[n][sum];
+        if(sum==0 && n==0)
         {
-            return 1; 
+            t[n][sum]=1;
+            return t[n][sum];
         }
-        if(n==0) return 0;
-        int sum1=sum-nums[n-1];
-        sum=sum+nums[n-1];
-        return rec(ans,nums,sum1,n-1,target)+rec(ans,nums,sum,n-1,target);
+        if(n==0)
+        {
+            t[n][sum]=0;
+            return t[n][sum];
+        }
+        if(sum>=nums[n-1])
+        {
+            t[n][sum]=rec(t,nums,n-1,sum)+rec(t,nums,n-1,sum-nums[n-1]);
+            return t[n][sum];
+        }
+        else{
+            t[n][sum]=rec(t,nums,n-1,sum);
+            return t[n][sum];
+        }
+
     }
-
-
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int sum=0;
-        int ans=0;
-        
-        return rec(ans,nums,sum,n,target);
+        int totalsum = accumulate(nums.begin(),nums.end(),0);
+        if(abs(target)>totalsum) return abs(target)==totalsum;
+        target=totalsum-abs(target);
+        if(target%2) return 0;
+        target/=2;
+        vector<vector<int>>t(n+1,vector<int>(target+1,0));
+        for(int i=0;i<n+1;i++)
+            for(int j=0;j<target+1;j++)
+                t[i][j]=-1;
+        rec(t,nums,n,target);
+        // for(int i=0;i<n+1;i++)
+        // {
+        //     for(int j=0;j<target+1;j++)
+        //         cout<<t[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        return t[n][target];
+
+
     }
 };
