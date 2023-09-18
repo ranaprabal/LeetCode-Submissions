4007d4153b706358ef6a3002ade4f5726369c177
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-18 11:16:43 +0530

    Time: 0 ms (100%), Space: 6.4 MB (9.11%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+      int a[n];
+			if(n<4) return n;
+			a[0]=1;
+			a[1]=2;
+			for(int i=2;i<n;i++){
+				a[i]=a[i-1]+a[i-2];
+			}
+			return a[n-1];
+	
+    }
+};
