4a0504b022e2ba93a3d63f481dc435d793a62d8f
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-29 00:52:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -19,6 +19,13 @@ public:
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
         int n=nums.size();
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[n];
+        
         return rec(n,nums);
     }
 };
