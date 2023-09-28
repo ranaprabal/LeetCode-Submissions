8bf9b611508d136907d771841efe5327c4f015be
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-29 00:59:12 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,13 @@
-int static dp[101];
 class Solution {
-public:
-    int rec(int n,vector<int>&nums){
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        if(n==0){
-            dp[n]=0;
-            return dp[n];
-        }
-        if(n==1){
-            dp[n]=nums[0];
-            return dp[n];
-        }
-        dp[n] = max(rec(n-1,nums),rec(n-2,nums)+nums[n-1]);
-        return dp[n];
-    }
+public:    
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int n=nums.size();
+        int dp[n+1];
         dp[0]=0;
         dp[1]=nums[0];
         for(int i=2;i<n+1;i++){
             dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
         }
         return dp[n];
-        
-        return rec(n,nums);
     }
 };
