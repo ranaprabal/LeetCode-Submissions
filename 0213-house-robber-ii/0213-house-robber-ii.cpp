866b71afb07e59157e5784a3bf12e5f111e87d0b
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-29 01:38:43 +0530

    Time: 0 ms (100%), Space: 8 MB (31.12%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,35 +1,8 @@
-int static dp[101][2];
 class Solution {
 public:
-    int rec(int n,vector<int>&nums,int use){
-        if(dp[n][use]!=-1){
-            return dp[n][use];
-        }
-        if(n==0){
-            dp[n][use]=0;
-            return dp[n][use];
-        }
-        if(n==1 && use==0){
-            dp[n][use]=nums[0];
-            return dp[n][use];
-        }
-        if(n==1 && use==1){
-            dp[n][use]=0;
-            return dp[n][use];
-        }
-        if(n==nums.size()){
-            dp[n][use] = max(rec(n-1,nums,0),rec(n-2,nums,1)+nums[n-1]);
-            return dp[n][use];    
-        }
-        else{
-            dp[n][use] = max(rec(n-1,nums,use),rec(n-2,nums,use)+nums[n-1]);
-            return dp[n][use];
-        }
-    }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-
         int n=nums.size();
+        int dp[n+1][2];
         dp[0][1]=dp[0][0]=0;
         dp[1][0]=nums[0];
         dp[1][1]=0;
@@ -44,6 +17,5 @@ public:
             }
         }
         return dp[n][0];
-        return rec(n,nums,0);
     }
 };
