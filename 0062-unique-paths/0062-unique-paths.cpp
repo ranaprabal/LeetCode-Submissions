6e4fba97456bbbab5f8a8111b7c3bb4e6c53134e
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-09-29 15:08:31 +0530

    Time: 0 ms (100%), Space: 6.4 MB (59.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,18 @@
+int static dp[101][101];
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        int dp[m][n];
-        for(int i=0;i<m;i++) dp[i][0]=1;
-        for(int i=0;i<n;i++) dp[0][i]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+    int rec(int i,int j){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==0 && j==0) {
+            dp[i][j]=1;
+            return dp[i][j];
         }
-        return dp[m-1][n-1];
+        dp[i][j] = rec(i-1,j)+rec(i,j-1);
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        memset(dp,-1,sizeof(dp));
+        return rec(m-1,n-1);
     }
 };
