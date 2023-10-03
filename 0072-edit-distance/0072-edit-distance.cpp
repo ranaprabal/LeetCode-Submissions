9e8f0273878c83f43af66c948d01199536a1bd9b
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-10-04 01:20:36 +0530

    Time: 23 ms (24.28%), Space: 7.6 MB (81.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,7 +2,7 @@ int dp[501][501];
 class Solution {
     int rec(int n,int m,string &str1,string &str2){
         if(dp[n][m]!=-1) return dp[n][m];
-        if(n==0 || m==0) return dp[n][m] = max(n+1,m+1);
+        if(n==0 || m==0) return dp[n][m] = max(n,m);
         if(str1[n-1]==str2[m-1]){
             return dp[n][m] = rec(n-1,m-1,str1,str2);
         }    
@@ -13,6 +13,6 @@ public:
         memset(dp,-1,sizeof(dp));
         int n=str1.size();
         int m = str2.size();
-        return rec(n,m,str1,str2)-1;
+        return rec(n,m,str1,str2);
     }
 };
