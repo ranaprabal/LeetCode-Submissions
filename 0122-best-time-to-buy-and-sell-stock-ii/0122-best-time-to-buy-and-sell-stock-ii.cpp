0faa311c4229a52a6f00baa096a4f43247bd380a
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-10-04 16:17:38 +0530

    Time: 11 ms (18.8%), Space: 15.7 MB (19.09%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,19 @@
+static int dp[30001][2];
 class Solution {
 public:
+    int rec(int i,bool buy,vector<int>&a){
+        if(i==a.size()) return dp[i][buy] = 0;
+        if(dp[i][buy]!=-1) return dp[i][buy]; 
+        if(buy){
+            return dp[i][buy] = max( -a[i] + rec(i+1,0,a) , rec(i+1,1,a));
+        }
+        else{
+            return dp[i][buy] = max(a[i]+rec(i+1,1,a),rec(i+1,0,a));
+        }
+    }
     int maxProfit(vector<int>& a) {
         int n=a.size();
-        int ans =0,prev=a[0];
-        for(int i=0;i<n;i++){
-            if(0<a[i]-prev){
-                ans+=a[i]-prev;
-                prev=a[i];
-            }
-            else prev=a[i];
-        }
-        return ans;
+        memset(dp,-1,sizeof(dp));
+        return rec(0,1,a);
     }
 };
