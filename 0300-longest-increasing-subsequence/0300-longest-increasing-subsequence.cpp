bc915f06c2555f7c038e2911f07f165df9bdc07b
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-10-05 01:38:25 +0530

    Time: 447 ms (33.31%), Space: 38.4 MB (32.17%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -12,17 +12,32 @@ public:
         } 
         return dp[n][prev] = rec(n-1,prev,a);
     }
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> x;
-        for (int num : nums) {
-            auto it = lower_bound(x.begin(), x.end(), num);
-            if (it == x.end()) {
-                x.push_back(num);
-            } else {
-                *it = num;
-            }
-        }
-        return x.size();
+    int lengthOfLIS(vector<int>& a) {
+        int n = a.size();
+        int dp[n+1][n+1];
+	for(int i=0;i<n+1;i++) dp[0][i]=0;
+	for(int i=1;i<n+1;i++){
+		for(int j=0;j<n+1;j++){
+			if(j==n){
+			    dp[i][j] = max(1+dp[i-1][i-1],dp[i-1][j]);
+			}
+			else if(a[j]>a[i-1]){
+			    dp[i][j]=max(1+dp[i-1][i-1],dp[i-1][j]);
+			}
+			else dp[i][j] = dp[i-1][j];
+		}
+	}
+    return dp[n][n];
+        // vector<int> x;
+        // for (int num : nums) {
+        //     auto it = lower_bound(x.begin(), x.end(), num);
+        //     if (it == x.end()) {
+        //         x.push_back(num);
+        //     } else {
+        //         *it = num;
+        //     }
+        // }
+        // return x.size();
         // int n=nums.size();
         // memset(dp,-1,sizeof(dp));
         // return rec(n,n,nums);
