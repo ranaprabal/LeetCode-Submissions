ef4561289c5961f0276fb933c38dd166c982a3a8
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-10-16 22:54:19 +0530

    Time: 151 ms (25.52%), Space: 27.1 MB (42.46%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,30 +1,25 @@
 bool cmp(const string lhs, const string rhs){
     return lhs.length() < rhs.length();
 }
+
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
-        int n=words.size();
+        map<string,int>mpp;
+        int ans=1;
         sort(words.begin(),words.end(),cmp);
-        vector<int>length(n,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                bool found=false;
-                if(words[i].size()==words[j].size()+1){
-                    for(int p=0;p<words[i].size();p++){
-                        if(words[i].substr(0,p)+words[i].substr(p+1)==words[j]){
-                            found=true;
-                            break;
-                        }
-                    }
-                }
-                if(found){
-                    length[i]=max(length[i],length[j]+1);
+        for(auto &it:words)
+        {
+            mpp[it]=1;
+            for(int i=0;i<it.size();i++){
+                string temp=it.substr(0,i)+it.substr(i+1);
+                if(mpp.find(temp)!=mpp.end()){
+                    mpp[it]=max(mpp[it],mpp[temp]+1);
                 }
             }
+            ans=max(ans,mpp[it]);
         }
-        return *max_element(length.begin(),length.end());
-
+        return ans;
         
     }
 };
