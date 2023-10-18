63cf7eb0b7316c027e9222d05b526884f575ad73
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2023-10-18 14:44:09 +0530

    Time: 35 ms (93.35%), Space: 8.7 MB (91.98%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    unordered_map<string,int>dp;
+    int dp[101][101];
     int rec(vector<int>&cuts,int i,int j,int m,int n){
         if(i>=j) return 0;
-        string key = to_string(i)+" "+to_string(j);
-        if(dp.find(key)!=dp.end()) return dp[key];
+        if(dp[i][j]!=-1) return dp[i][j];
         int temp =INT_MAX;
         for(int k=i;k<j;k++){
             temp=min(temp, n-m + rec(cuts,i,k,m,cuts[k])+rec(cuts,k+1,j,cuts[k],n));
         }
-        return dp[key]=temp;
+        return dp[i][j]=temp;
     }
     int minCost(int n, vector<int>& cuts) {
+        memset(dp,-1,sizeof(dp));
         sort(cuts.begin(),cuts.end());
         return rec(cuts,0,cuts.size(),0,n);
     }
