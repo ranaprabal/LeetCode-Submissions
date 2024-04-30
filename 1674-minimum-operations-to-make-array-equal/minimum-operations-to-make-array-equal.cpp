a7a63746cbadfd9414a7f513e72aa714420f0aaf
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2024-04-30 14:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (67.71%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int minOperations(int n) {
-        if(n&1){
-            return (n+1)*(n-1)/4;
-        }
-        else return (n*n)/4;
+        return (n*n - (n&1))/4;
     }
 };
