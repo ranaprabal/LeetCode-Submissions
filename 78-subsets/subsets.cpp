f1b70e32a945f2b6e8b20628bb00ba8865b5b3ab
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2024-05-21 22:05:43 +0530

    Time: 7 ms (6.48%) | Memory: 14 MB (21.93%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -7,10 +7,9 @@ public:
             ans.push_back(op);
             return;
         }
-        vector<int>op2=op;
-        op2.push_back(ip[i]);
         cal(i+1,op,ip,ans);
-        cal(i+1,op2,ip,ans);
+        op.push_back(ip[i]);
+        cal(i+1,op,ip,ans);
 
     }
     vector<vector<int>> subsets(vector<int>& nums) {
