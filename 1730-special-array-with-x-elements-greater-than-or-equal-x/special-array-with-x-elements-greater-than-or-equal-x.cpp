1bfa81235836fbfd4b44cc27b1aed380fb68a01e
Prabal Pratap <96778132+ranaprabal@users.noreply.github.com>
2024-05-27 13:07:01 +0530

    Time: 2 ms (51.71%) | Memory: 10.2 MB (19.80%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -5,6 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<1000;i++){
             auto temp = lower_bound(nums.begin(),nums.end(),i) - nums.begin();
+            if(temp==nums.size()) break;
             // cout<<temp<<" "<<i<<" "<<bool(i==(nums.size()-temp))<<endl;
             if(i==nums.size()-temp){
                 ans=i;
